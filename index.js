9de69ea6bc30b42713b17bd3e1223a5d723ebfd4
Neven Bedr <neven2225@gmail.com>
2020-07-09 19:32:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
 class Formatter {
-  static capitalize(s) {
-    if (typeof s !== 'string') return ''
-    return s.charAt(0).toUpperCase() + s.slice(1)
-  }
-  static sanitize(f) {
-    const chars = []
-    f.split('').forEach((char) => {
-      if (!['@', '#', '!', '$', '%', '^', '{', '}', '*', '(', ')'].includes(char) || ["'"].includes(char)) {
-        chars.push(char)
-      }
-    })
-
-    return chars.join('')
-  }
-  static titleize(sent) {
-    const words = []
-    sent.split(' ').forEach((word, index) => {
-      if (index === 0 || !['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'].includes(word)) {
-        words.push(this.capitalize(word))
-      } else {
-        words.push(word)
-      }
-    })
-
-    return words.join(' ')
-  }
-
-
+  //add static methods here
 }
+
