730c57117ee89fb50e6e892813f5bf3651c80c14
Neven Bedr <neven2225@gmail.com>
2020-07-09 19:33:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+
 class Formatter {
-  //add static methods here
+    //add static methods here
+  static capitalize(s) {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+  static sanitize(f) {
+    const chars = []
+    f.split('').forEach((char) => {
+      if (!['@', '#', '!', '$', '%', '^', '{', '}', '*', '(', ')'].includes(char) || ["'"].includes(char)) {
+        chars.push(char)
+      }
+    })
+
+    return chars.join('')
+  }
+  static titleize(sent) {
+    const words = []
+    sent.split(' ').forEach((word, index) => {
+      if (index === 0 || !['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'].includes(word)) {
+        words.push(this.capitalize(word))
+      } else {
+        words.push(word)
+      }
+    })
+
+    return words.join(' ')
+  }
+
+
 }
